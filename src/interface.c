af05ce498454e8b001bd5b9c18ea6b9d85aa7391
Kaushal Kumar <kaushal@src.gnome.org>
2003-12-11 13:26:41 +0000

    Fix compilation

--- src/interface.c
+++ src/interface.c
@@ -395,6 +395,7 @@ create_sys_view (ProcData *procdata)
 	sizegroup = gtk_size_group_new (GTK_SIZE_GROUP_HORIZONTAL);
 	for (i=0;i<procdata->config.num_cpus; i++) {
 		GtkWidget *temp_hbox;
+		gchar *text;
 		/* Two per row */
 		if (fabs(fmod(i,2) - 0) < .01) {
 			hbox = gtk_hbox_new (FALSE, 12);
@@ -416,7 +417,7 @@ create_sys_view (ProcData *procdata)
 			    G_CALLBACK (cb_cpu_color_changed), GINT_TO_POINTER (i));
 		gtk_box_pack_start (GTK_BOX (temp_hbox), color_picker, FALSE, FALSE, 0);
 		
-		gchar *text = g_strdup_printf (_("CPU%d:"), i+1);
+		text = g_strdup_printf (_("CPU%d:"), i+1);
 		label = gtk_label_new (text);
 		gtk_box_pack_start (GTK_BOX (temp_hbox), label, FALSE, FALSE, 0);
 		g_free (text);
