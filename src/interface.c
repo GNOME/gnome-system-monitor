06e542977e4dadb11ed51e7c94df651f7b535fa5
Benoît Dejean <bdejean@src.gnome.org>
2005-03-14 09:17:26 +0000

    don't use C++-style comments. Patch by jensgr@gmx.net (Jens Granseuer).
    	* src/interface.c: (create_sys_view):
    	* src/openfiles.c: (compare_open_files): don't use C++-style comments.
    	Patch by jensgr@gmx.net (Jens Granseuer).

--- src/interface.c
+++ src/interface.c
@@ -540,12 +540,12 @@ create_sys_view (ProcData *procdata)
 
 	model = gtk_tree_store_new (10,
 				    GDK_TYPE_PIXBUF,
-				    G_TYPE_STRING, // device name
-				    G_TYPE_STRING, // directory
-				    G_TYPE_STRING, // type
-				    G_TYPE_STRING, // total
-				    G_TYPE_STRING, // free
-				    G_TYPE_STRING, // used
+				    G_TYPE_STRING, /* device name */
+				    G_TYPE_STRING, /* directory */
+				    G_TYPE_STRING, /* type */
+				    G_TYPE_STRING, /* total */
+				    G_TYPE_STRING, /* free */
+				    G_TYPE_STRING, /* used */
 				    G_TYPE_FLOAT,
 				    G_TYPE_FLOAT,
 				    G_TYPE_FLOAT);
