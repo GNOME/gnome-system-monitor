9ff01ab4fa24d28e9e189e5cbfc25af36e02720f
James Henstridge <james@jamesh.id.au>
2005-12-12 11:22:27 +0000

    disable gconf schema install if $DESTDIR is set.
    2005-12-12  James Henstridge  <james@jamesh.id.au>
    
    	* src/Makefile.am: disable gconf schema install if $DESTDIR is
    	set.
    
    	* Makefile.am: ignore scrollkeeper database in the
    	distuninstallcheck.  Remove intltool programs on distclean.
    
    	* configure.in: modernise a bit, and add AC_DEFINE() descriptions
    	so that things work without acconfig.h.
    
    	* acconfig.h: remove obsolete file.
    
    	* autogen.sh: require Automake 1.9.

--- autogen.sh
+++ autogen.sh
@@ -16,4 +16,6 @@ which gnome-autogen.sh || {
     echo "You need to install gnome-common from the GNOME CVS"
     exit 1
 }
-USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
+REQUIRED_AUTOMAKE_VERSION=1.9
+USE_COMMON_DOC_BUILD=yes
+. gnome-autogen.sh
