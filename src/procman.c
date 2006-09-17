1e484edc7dfad001cdb4f9359fba5fb8ed9189a3
Benoît Dejean <benoit@placenet.org>
2006-09-17 10:17:50 +0000

    Fixed locales dir initialization.
    2006-09-17  Benoît Dejean  <benoit@placenet.org>
    
    	* src/Makefile.am:
    	* src/procman.c: (main):
    
    	Fixed locales dir initialization.

--- src/procman.c
+++ src/procman.c
@@ -659,7 +659,7 @@ main (int argc, char *argv[])
 	ProcData *procdata;
 	BaconMessageConnection *conn;
 
-	bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
+	bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
 	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
 	textdomain (GETTEXT_PACKAGE);
 
