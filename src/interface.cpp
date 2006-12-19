05f08bf747b11cc2379150b6ffe0601a0d581f77
Benoît Dejean <benoit@placenet.org>
2006-12-19 20:18:37 +0000

    Fixed build and released 2.17.4.2.
    2006-12-19  Benoît Dejean  <benoit@placenet.org>
    
    	* NEWS:
    	* configure.in:
    	* src/interface.cpp:
    
    	Fixed build and released 2.17.4.2.

--- src/interface.cpp
+++ src/interface.cpp
@@ -654,9 +654,9 @@ disconnect_proxy_cb (GtkUIManager *manager,
 {
 	if (GTK_IS_MENU_ITEM (proxy)) {
 		g_signal_handlers_disconnect_by_func
-			(proxy, reinterpret_cast<void*>(G_CALLBACK(menu_item_select_cb)), procdata);
+			(proxy, (void*)(G_CALLBACK(menu_item_select_cb)), procdata);
 		g_signal_handlers_disconnect_by_func
-			(proxy, reinterpret_cast<void*>(G_CALLBACK(menu_item_deselect_cb)), procdata);
+			(proxy, (void*)(G_CALLBACK(menu_item_deselect_cb)), procdata);
 	}
 }
 
