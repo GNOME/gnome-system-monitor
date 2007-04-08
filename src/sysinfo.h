7515afe26c33d27f0b0fd0506097d856b7bec78c
Benoît Dejean <bdejean@src.gnome.org>
2007-04-08 21:37:39 +0000

    Moved tag where it belongs to.
    svn path=/tags/git-migration-GNOME_SYSTEM_MONITOR_2_17_94; revision=1987

--- src/sysinfo.h
+++
@@ -1,9 +0,0 @@
-#ifndef H_GNOME_SYSTEM_MONITOR_SYSINFO_H_1155594649
-#define H_GNOME_SYSTEM_MONITOR_SYSINFO_H_1155594649
-
-#include <gtk/gtk.h>
-
-GtkWidget *
-procman_create_sysinfo_view(void);
-
-#endif /* H_GNOME_SYSTEM_MONITOR_SYSINFO_H_1155594649 */
