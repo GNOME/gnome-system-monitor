95d12b08c62b4abb4334316232cc458e838d0ec6
Benoît Dejean <benoit@placenet.org>
2009-05-12 19:41:18 +0200

    Gconf key and hook for network_in_bits.

--- src/gconf-keys.cpp
+++ src/gconf-keys.cpp
@@ -8,6 +8,7 @@ namespace procman
     const std::string root("/apps/procman");
     const std::string solaris_mode(root + "/solaris_mode");
     const std::string open_files_tree_prefix(root + "/openfilestree");
+    const std::string network_in_bits(root + "/network_in_bits");
   }
 }
 
