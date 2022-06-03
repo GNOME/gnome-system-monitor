863f6529a049bdaaca98b2c526c4433954c711b1
Ondřej Míchal <harrymichal@seznam.cz>
2022-06-03 03:42:00 +0300

    setaffinity: Stop using GtkStyleContext for setting a CSS class

--- src/setaffinity.cpp
+++ src/setaffinity.cpp
@@ -271,7 +271,6 @@ create_single_set_affinity_dialog (GtkTreeModel *model,
   GtkWidget       *dialog_vbox;
   GtkWidget       *label;
   GtkWidget       *scrolled;
-  GtkStyleContext *scrolled_style;
   GtkGrid         *cpulist_grid;
 
   guint16               *affinity_cpus;
@@ -359,8 +358,7 @@ create_single_set_affinity_dialog (GtkTreeModel *model,
   gtk_widget_set_vexpand (GTK_WIDGET (scrolled), TRUE);
 
   /* Add view class to scrolled box style */
-  scrolled_style = gtk_widget_get_style_context (scrolled);
-  gtk_style_context_add_class (scrolled_style, "view");
+  gtk_widget_add_css_class (scrolled, "view");
 
   /* Set scrolled box vertical and horizontal policies */
   gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled),
