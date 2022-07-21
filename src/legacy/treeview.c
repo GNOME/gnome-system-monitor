599abec05dd3a062cbf189f0f0e1390f058900be
Ondřej Míchal <harrymichal@seznam.cz>
2022-07-21 02:01:32 +0200

    legacy/treeview: Save a column only if it exists
    I encountered an issue when closing the application which was probably
    caused by TreeView misconfiguration during the GTK4 work which caused
    a column to be misconfigured/incomplete. That would cause a crash during
    the close sequence. So, be extra defensive and only save an existing
    column.

--- src/legacy/treeview.c
+++ src/legacy/treeview.c
@@ -67,18 +67,21 @@ gsm_tree_view_save_state (GsmTreeView *tree_view)
       GList *iter;
       GVariantBuilder builder;
 
-      g_variant_builder_init (&builder, G_VARIANT_TYPE_ARRAY);
-
-      for (iter = columns; iter != NULL; iter = iter->next)
+      if (columns != NULL)
         {
-          gint id = gtk_tree_view_column_get_sort_column_id (GTK_TREE_VIEW_COLUMN (iter->data));
-          g_variant_builder_add (&builder, "i", id);
+          g_variant_builder_init (&builder, G_VARIANT_TYPE_ARRAY);
+
+          for (iter = columns; iter != NULL; iter = iter->next)
+            {
+              gint id = gtk_tree_view_column_get_sort_column_id (GTK_TREE_VIEW_COLUMN (iter->data));
+              g_variant_builder_add (&builder, "i", id);
+            }
+
+          g_settings_set_value (priv->settings, "columns-order",
+                                g_variant_builder_end (&builder));
+
+          g_list_free (columns);
         }
-
-      g_settings_set_value (priv->settings, "columns-order",
-                            g_variant_builder_end (&builder));
-
-      g_list_free (columns);
     }
 
   g_settings_apply (priv->settings);
