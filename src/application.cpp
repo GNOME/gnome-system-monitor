8e97fe0dac7edd3935ae539f0ec2955d10668d21
oscfdezdz <oscfdezdz@tuta.io>
2024-08-16 15:17:12 +0200

    application: Load settings once
    load_settings is already called a few lines below.

--- src/application.cpp
+++ src/application.cpp
@@ -518,7 +518,6 @@ GsmApplication::on_startup ()
   glibtop_init ();
 
   load_resources ();
-  load_settings ();
 
   style_manager = adw_style_manager_get_default ();
   adw_style_manager_set_color_scheme (style_manager, ADW_COLOR_SCHEME_PREFER_LIGHT);
