GNOME/gnome-system-monitor

commit 82d5ce7921d76df1dc43626fb2ec4e3dd1a6dede

interface: Use the suspended signal in GtkWindow to pause graph drawing

src/interface.cpp
@@ -738,14 +738,13 @@ cb_main_window_delete (GtkWindow *window,
 }
 
 static gboolean
-cb_main_window_state_changed (GdkToplevel      *surface,
-                              GdkToplevelState *state,
-                              GsmApplication   *app)
+cb_main_window_suspended (GtkWindow      *surface,
+                          gboolean        suspended,
+                          GsmApplication *app)
 {
   auto current_page = app->config.current_tab;
 
-  if (*state & GDK_TOPLEVEL_STATE_BELOW ||
-      *state & GDK_TOPLEVEL_STATE_MINIMIZED)
+  if (suspended)
     {
       if (current_page == "processes")
         {
@@ -781,7 +780,6 @@ cb_main_window_state_changed (GdkToplevel      *surface,
           disks_thaw (app);
         }
     }
-  return FALSE;
 }
 
 void
@@ -858,9 +856,10 @@ create_main_window (GsmApplication *app)
                     "notify::visible-child",
                     G_CALLBACK (cb_change_current_page),
                     app);
+  g_signal_connect (G_OBJECT (app->main_window),
+                    "notify::suspended",
+                    G_CALLBACK (cb_main_window_suspended),
                     app);
-  g_signal_connect (G_OBJECT (main_window), "window-state-event",
-                    G_CALLBACK (cb_main_window_state_changed),
   g_signal_connect (G_OBJECT (app->main_window),
                     "destroy",
                     G_CALLBACK (cb_main_window_delete),

commit 714faa9fad2a41f6353a242d48e5cb59b6ecdaac

interface: Fix resuming from suspended state
The second parameter of the callback function is a GParamSpec, not a boolean.

Fixes: 82d5ce7921d76df1dc43626fb2ec4e3dd1a6dede

Closes: https://gitlab.gnome.org/GNOME/gnome-system-monitor/-/issues/304

src/interface.cpp
@@ -754,13 +754,13 @@ cb_main_window_delete (GtkWindow*,
 }
 
 static void
-cb_main_window_suspended (GtkWindow*,
-                          gboolean        suspended,
+cb_main_window_suspended (GtkWindow      *window,
+                          GParamSpec     *pspec,
                           GsmApplication *app)
 {
   auto current_page = app->config.current_tab;
 
-  if (suspended)
+  if (gtk_window_is_suspended (window))
     {
       if (current_page == "processes")
         {