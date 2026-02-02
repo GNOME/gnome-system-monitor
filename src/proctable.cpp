73991da3f02f545ad01d877ed4575321afee3dbf
Zander Brown <zbrown@gnome.org>
2026-02-02 01:01:37 +0000

    cgroups: Move to an isolated module
    Continue our efforts to seperate concerns, additionally suppress the
    category tags on groups that are untagged.

--- src/proctable.cpp
+++ src/proctable.cpp
@@ -50,7 +50,7 @@
 #include "procinfo.h"
 #include "gsm-selinux.h"
 #include "settings-keys.h"
-#include "cgroups.h"
+#include "gsm-cgroups.h"
 #include "legacy/treeview.h"
 
 #ifdef HAVE_SYSTEMD
@@ -661,8 +661,9 @@ proctable_new (GsmApplication * const app)
   app->last_vscroll_max = 0;
   app->last_vscroll_value = 0;
 
-  if (!cgroups_enabled ())
+  if (!gsm_cgroups_is_enabled ()) {
     gsm_tree_view_add_excluded_column (proctree, COL_CGROUP);
+  }
 
 #ifdef HAVE_SYSTEMD
   if (!gsm_systemd_is_running ()) {
@@ -971,9 +972,7 @@ update_info (GsmApplication *app,
   g_assert (info->pid != info->ppid);
   g_assert (info->ppid != -1 || info->pid == 0);
 
-  /* get cgroup data */
-  get_process_cgroup_info (*info);
-
+  gsm_proc_info_load_cgroups (info);
   gsm_proc_info_load_systemd (info);
 }
 
