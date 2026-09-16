cf30a3cd45b7fa42bdefa9d579933ecd8de3fe61
Zander Brown <zbrown@gnome.org>
2026-09-16 02:54:21 +0100

    selinux: Use new helpers

--- src/selinux/gsm-selinux.c
+++ src/selinux/gsm-selinux.c
@@ -6,6 +6,8 @@
 
 #include <glib.h>
 
+#include "gsm-module-loader.h"
+
 #include "gsm-selinux.h"
 
 
@@ -14,78 +16,26 @@ static void (*freecon) (char *);
 static int (*is_selinux_enabled) (void);
 
 
-static inline GModule *
-load_selinux_module (void)
+GSM_DEFINE_MODULE_LOADER (gsm, selinux, "libselinux.so.1")
+
+
+static inline GsmModuleInitResult
+gsm_selinux_init_module (GModule *module)
 {
-  g_autoptr (GError) error = NULL;
-  GModule *module =
-    g_module_open_full ("libselinux.so.1",
-                        G_MODULE_BIND_LAZY | G_MODULE_BIND_LOCAL,
-                        &error);
-
-  if (error) {
-    g_debug ("Could not load libselinux.so.1: %s", error->message);
-    goto fail;
-  }
-
-  if (!module) {
-    g_debug ("Could not load libselinux.so.1");
-    goto fail;
-  }
-
-  if (!g_module_symbol (module, "getpidcon", (gpointer *) &getpidcon)) {
-    g_debug ("Could not load getpidcon from libselinux.so.1");
-    goto fail;
-  } else {
-    g_debug ("Loaded getpidcon from libselinux.so.1");
-  }
-
-  if (!g_module_symbol (module, "freecon", (gpointer *) &freecon)) {
-    g_debug ("Could not load freecon from libselinux.so.1");
-    goto fail;
-  } else {
-    g_debug ("Loaded freecon from libselinux.so.1");
-  }
-  
-  if (!g_module_symbol (module,
-                        "is_selinux_enabled",
-                        (gpointer *) &is_selinux_enabled)) {
-    g_debug ("Could not load is_selinux_enabled from libselinux.so.1");
-    goto fail;
-  } else {
-    g_debug ("Loaded is_selinux_enabled from libselinux.so.1");
-  }
+  GSM_MODULE_REQUIRE_SYMBOL (module, "libselinux.so.1", getpidcon);
+  GSM_MODULE_REQUIRE_SYMBOL (module, "libselinux.so.1", freecon);
+  GSM_MODULE_REQUIRE_SYMBOL (module, "libselinux.so.1", is_selinux_enabled);
 
   g_module_make_resident (module);
 
-  return g_steal_pointer (&module);
-
-fail:
-  g_clear_pointer (&module, g_module_close);
-
-  return NULL;
-}
-
-
-static inline GModule *
-get_selinux_module (void)
-{
-  static GModule *module = NULL;
-
-  if (g_once_init_enter_pointer (&module)) {
-    GModule *gksu = load_selinux_module ();
-
-    g_once_init_leave_pointer (&module, g_steal_pointer (&gksu));
-  }
-
-  return module;
+  return GSM_MODULE_INIT_SUCCESS;
 }
 
 
 gboolean
 gsm_selinux_is_enabled (void)
 {
-  if (get_selinux_module () == NULL) {
+  if (!gsm_selinux_get_or_load_module ()) {
     return FALSE;
   }
 
@@ -111,7 +61,7 @@ gsm_selinux_get_context (pid_t pid)
   g_autofree char *result = NULL;
   char *con = NULL;
 
-  if (get_selinux_module () == NULL) {
+  if (!gsm_selinux_get_or_load_module ()) {
     goto out;
   }
 
